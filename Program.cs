fd94071e384ea1744c28902e00b23f9ded5ff93a
Jared Burnham <jaredburnham@Jareds-MacBook-Pro.local>
2022-10-02 18:11:06 -0500

    Ver1.1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//main
+
+
+//problem 1
+for(int i = 11; i > 1; i--)
+{
+    for(int j = i; j > 1; j--){
+        Console.Write("*");
+    }
+    Console.Write("\n");
+}
+
+//formatting
+for(int k = 0; k < 2; k++)
+{
+Console.Write("\n");
+}
+
+
+//problem 2
+for(int i = 1; i < 5; i++)
+{
+    Console.Write(i + "\t");
+    for(int j = 0; j < 10; j++)
+    {
+    Console.Write((i * j) + "\t");
+    }
+    Console.Write("\n");
+}
